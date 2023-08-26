8914a5b82365717bb72072a7edea8c6fdbbf8a22
Hande <96186094+handeerdal@users.noreply.github.com>
2023-08-26 14:45:20 +0300

    Add files via upload

---
+++ I_am_Rich/I_am_Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I_am_Rich
+//
+//  Created by Hande Erdal on 2.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
